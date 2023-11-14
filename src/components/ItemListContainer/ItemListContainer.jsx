77c17af5aaeafcef2ea0d6cbadbd48b45c532015
Nicolas Cicchini <nicolase.cicchini@gmail.com>
2023-11-14 19:44:54 -0300

    ItemListContainer bug fixed

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -11,31 +11,25 @@ export const ItemListContainer = () => {
   const [products, setProducts] = useState([])
   const { cid } = useParams()
 
-
-  //traer muchos
   useEffect(() => {
-    const dbFirestore = getFirestore()
-    const queryCollection = collection(dbFirestore, 'products')
-    getDocs(queryCollection)
-      .then(result => setProducts(result.docs.map(products => ({ id: products.id, ...products.data() }))))
-      .catch(error => console.log(error))
+    if (cid) {
+      const dbFirestore = getFirestore()
+      const queryCollection = collection(dbFirestore, 'products')
+      const queryFilter = query(queryCollection, where('category', '==', cid))
+      
+      getDocs(queryFilter)
+        .then(result => setProducts(result.docs.map(products => ({ id: products.id, ...products.data() }))))
+        .catch(error => console.log(error))
 
-  }, [])
-  console.log(products)
+    } else {
+      const dbFirestore = getFirestore()
+      const queryCollection = collection(dbFirestore, 'products')
 
-  // traer muchos pero filtrados
-  useEffect(() => {
-    const dbFirestore = getFirestore()
-    const queryCollection = collection(dbFirestore, 'products')
-    const queryFilter = query(queryCollection, where('category', '==', cid))
-
-
-    getDocs(queryFilter)
-      .then(result => setProducts(result.docs.map(products => ({ id: products.id, ...products.data() }))))
-      .catch(error => console.log(error))
+      getDocs(queryCollection)
+        .then(result => setProducts(result.docs.map(products => ({ id: products.id, ...products.data() }))))
+        .catch(error => console.log(error))
+    }
   }, [cid])
-  console.log(products)
-
   return (
     <div>
       <ItemList products={products} />
