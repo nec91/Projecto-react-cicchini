b2ec6209dfdd3bfe7c1c5dde11479229184b04b8
Nicolas Cicchini <nicolase.cicchini@gmail.com>
2023-12-02 14:04:46 -0300

    actualizacion git ignore

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -8,6 +8,7 @@ import { doc, getDoc, getFirestore } from "firebase/firestore"
 
 export const ItemDetailContainer = () => {
   const [product, setProduct] = useState({})
+  const [loading, setLoading] = useState(true)
   const { pid } = useParams()
 
   useEffect(() => {
@@ -16,12 +17,18 @@ export const ItemDetailContainer = () => {
   getDoc(queryDoc)
     .then(result => setProduct({ id: result.id, ...result.data() }))
     .catch(error => console.log(error))
+    .finally(() => setLoading(false))
 }, [])
 
 return (
-  <div >
+  <>
+  {
+    loading ? <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> : 
+  <div>
     <ItemDetail product={product} />
   </div>
+  }
+  </>
 )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,25 +9,12 @@ export const NavBar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Frost-App</Navbar.Brand>
+            <NavLink  to="/">Frost-App</NavLink>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <NavLink className={({ isActive }) => isActive ? 'btn btn-primary' : 'btn'} to="/">Home</NavLink>
-                        <NavDropdown title="Servicios" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">
-                                Instalaciones
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Matenimiento preventivo
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">
-                                Servicio técnico
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">
-                                Venta de equipos
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        
                         <NavDropdown title="Categorías" id="collapsible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">
                                 <NavLink className={({ isActive }) => isActive ? 'btn btn-primary' : 'btn'} to="/category/Inverter">Inverter</NavLink>
